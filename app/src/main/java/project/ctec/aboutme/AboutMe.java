337c00d891db085d610b2529b93f6ecf2e2cc39d
Traitortots <jker3169@740s-pl-116.local>
2015-09-29 09:47:16 -0600

    Added Images, implemented images, implemented strings, created new methods

--- app/src/main/java/project/ctec/aboutme/AboutMe.java
+++ app/src/main/java/project/ctec/aboutme/AboutMe.java
@@ -4,34 +4,228 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.widget.ImageView;
 
-public class AboutMe extends AppCompatActivity {
+public class AboutMe extends AppCompatActivity
+{
+    // Defining the Buttons
+    private Button colorChangeButtton;
+    private Button backButton;
+    private Button schoolButton;
+    private Button gameButton;
+    private Button foodButton;
+    private Button campButton;
+    private Button programmingButton;
+
+    private RelativeLayout myLayout;
+
+    private TextView myText;
+    private ImageView myImage;
+
+    private int redColor;
+    private int blueColor;
+    private int greenColor;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
+
+        // Attaching my Buttons
+        colorChangeButtton = (Button) findViewById(R.id.colorChangeButton);
+        backButton = (Button) findViewById(R.id.backButton);
+        schoolButton = (Button) findViewById(R.id.schoolButton);
+        gameButton = (Button) findViewById(R.id.gameButton);
+        foodButton = (Button) findViewById(R.id.foodButton);
+        campButton = (Button) findViewById(R.id.campingButton);
+        programmingButton = (Button) findViewById(R.id.programmingButton);
+
+        myLayout = (RelativeLayout) findViewById(R.id.myLayout);
+
+        myText = (TextView) findViewById(R.id.textView);
+        myImage = (ImageView) findViewById(R.id.imageView);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_about_me, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Ran once to setup my listeners
+     */
+    public void setupListeners()
+    {
+        colorChangeButtton.setOnClickListener(new View.OnClickListener()
+        {
+
+            public void onClick(View v)
+            {
+                redColor = (int) (Math.random() * 256);
+                blueColor = (int) (Math.random() * 256);
+                greenColor = (int) (Math.random() * 256);
+
+                myLayout.setBackgroundColor(Color.rgb(redColor, blueColor, greenColor));
+            }
+
+        });
+        backButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                updateVisibility("Main");
+
+            }
+        });
+        schoolButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                updateVisibility("School");
+
+            }
+
+        });
+        gameButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                updateVisibility("Game");
+
+            }
+
+        });
+        foodButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                updateVisibility("Food");
+
+            }
+
+        });
+        campButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                updateVisibility("Camping");
+
+            }
+
+        });
+        programmingButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                updateVisibility("Programming");
+
+            }
+
+        });
+
+    }
+
+    /**
+     * Called to change the visibility of all to make the current screen
+     * @param currentScreen is the current screen you want: "Main", "School", "Game", "Food", "Camping", "Programming"
+     */
+    public void updateVisibility(String currentScreen)
+    {
+
+
+        if(currentScreen.equals("Main"))
+        {
+            updateButtonVisibility(true);
+            backButton.setVisibility(View.GONE);
+        }
+        else if(currentScreen.equals("School"))
+        {
+            updateButtonVisibility(false);
+            myImage.setBackgroundResource(R.drawable.back_to_school);
+            myText.setText(R.string.school_string);
+        }
+        else if(currentScreen.equals("Game"))
+        {
+            updateButtonVisibility(false);
+            myImage.setBackgroundResource(R.drawable.gaming_image);
+            myText.setText(R.string.game_string);
+
+        }
+        else if(currentScreen.equals("Food"))
+        {
+            updateButtonVisibility(false);
+            myImage.setBackgroundResource(R.drawable.food_image);
+            myText.setText(R.string.food_string);
+
+        }
+        else if(currentScreen.equals("Camping"))
+        {
+            updateButtonVisibility(false);
+            myImage.setBackgroundResource(R.drawable.camping_image);
+            myText.setText(R.string.camping_string);
+
+        }
+        else if(currentScreen.equals("Programming"))
+        {
+            updateButtonVisibility(false);
+            myImage.setBackgroundResource(R.drawable.coding);
+            myText.setText(R.string.code_string);
+        }
+
+    }
+
+    /**
+     * This is used to update wither the buttons are visible or not
+     * @param visible A boolean of wither you want them visible or not.
+     */
+    private void updateButtonVisibility(boolean visible)
+    {
+        if(visible)
+        {
+            myImage.setVisibility(View.GONE);
+            myText.setVisibility(View.GONE);
+            schoolButton.setVisibility(View.VISIBLE);
+            gameButton.setVisibility(View.VISIBLE);
+            foodButton.setVisibility(View.VISIBLE);
+            campButton.setVisibility(View.VISIBLE);
+            programmingButton.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            myImage.setVisibility(View.VISIBLE);
+            myText.setVisibility(View.VISIBLE);
+            schoolButton.setVisibility(View.GONE);
+            gameButton.setVisibility(View.GONE);
+            foodButton.setVisibility(View.GONE);
+            campButton.setVisibility(View.GONE);
+            programmingButton.setVisibility(View.GONE);
+            backButton.setVisibility(View.VISIBLE);
+        }
+
+    }
+
 }
